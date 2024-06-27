5fd0bb5f9f4622c6bc6ab1c18575695524fd0133
Arsen <a12341@Macbook-Air.local>
2024-06-27 11:53:52 +0400

    first commit

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,16 +13,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 5; // Количество пользователей для создания
+        $count = 5; 
 
         for ($i = 0; $i < $count; $i++) {
             $name = 'User ' . ($i + 1);
             $email = 'user' . ($i + 1) . '@example.com';
-            $password = 'aaaaaaaa'; // Ваше стандартное пароль
+            $password = 'aaaaaaaa'; 
 
             if (User::where('email', $email)->exists()) {
                 $this->command->error('Пользователь с таким email уже существует: ' . $email);
-                continue; // Пропускаем создание пользователя и переходим к следующей итерации цикла
+                continue;
             }
 
             User::create([
@@ -30,38 +30,10 @@ class UsersTableSeeder extends Seeder
                 'email' => $email,
                 'password' => Hash::make($password),
                 'private' => 'public',
-                'remember_token' => null, // Вы можете использовать Str::random(10) для генерации случайного токена
+                'remember_token' => null,
             ]);
 
             $this->command->info('Пользователь ' . ($i + 1) . ' создан успешно.');
         }
     }
 }
-
-
-// namespace Database\Seeders;
-
-// use Illuminate\Database\Seeder;
-// use Illuminate\Support\Facades\Hash;
-// use App\Models\User;
-
-// class UsersTableSeeder extends Seeder
-// {
-//     /**
-//      * Run the database seeds.
-//      */
-//     public function run()
-//     {
-//         $password = 'aaaaaaaa'; // Ваше стандартное пароль
-
-//         for ($i = 0; $i < 5; $i++) {
-//             User::create([
-//                 'name' => 'User ' . ($i + 1),
-//                 'email' => 'user' . ($i + 1) . '@example.com',
-//                 'password' => Hash::make($password),
-//                 'private' => 'public',
-//                 'remember_token' => null, // Вы можете использовать Str::random(10) для генерации случайного токена
-//             ]);
-//         }
-//     }
-// }
